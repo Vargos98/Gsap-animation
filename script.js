3e5641375eaffde4f6f9b2f57bd5c00182c76e46
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-06-29 09:04:43 +0530

    Created separate pages and their respective circle and also embedded gsap and gsap scroll-trigger scripts

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ var tl = gsap.timeline()
 
 
 tl.to('#box1',{
-  x:1200,
+  x:1350,
   rotate:360,
   scale:0.5,
   duration:2,
   delay:1
 })
 tl.to('#box2',{
-  x:1200,
+  x:1350,
   rotate:360,
   scale:0.5,
   duration:2,
@@ -18,10 +18,17 @@ tl.to('#box2',{
 })
 
 tl.to('#box3',{
-  x:1200,
+  x:1350,
   rotate:360,
   scale:0.5,
   duration:2,
 
+})
 
+
+gsap.from('#page1 #circle',{
+  scale:0,
+  delay:1,
+  duration:2,
+  rotate:720,
 })
