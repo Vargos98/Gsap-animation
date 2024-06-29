2952bbfd9c78760ec6117c905de8ed69b51a38ec
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-06-29 09:11:28 +0530

    Completed scrollTrigger animation

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-var tl = gsap.timeline()
+// var tl = gsap.timeline()
 
 
-tl.to('#box1',{
-  x:1350,
-  rotate:360,
-  scale:0.5,
-  duration:2,
-  delay:1
-})
-tl.to('#box2',{
-  x:1350,
-  rotate:360,
-  scale:0.5,
-  duration:2,
+// tl.to('#box1',{
+//   x:1350,
+//   rotate:360,
+//   scale:0.5,
+//   duration:2,
+//   delay:1
+// })
+// tl.to('#box2',{
+//   x:1350,
+//   rotate:360,
+//   scale:0.5,
+//   duration:2,
 
 
-})
+// })
 
-tl.to('#box3',{
-  x:1350,
-  rotate:360,
-  scale:0.5,
-  duration:2,
+// tl.to('#box3',{
+//   x:1350,
+//   rotate:360,
+//   scale:0.5,
+//   duration:2,
 
-})
+// })
 
 
 gsap.from('#page1 #circle',{
@@ -31,4 +31,31 @@ gsap.from('#page1 #circle',{
   delay:1,
   duration:2,
   rotate:720,
+
 })
+
+gsap.from('#page2 #circle',{
+  scale:0,
+  delay:1,
+  duration:2,
+  rotate:720,
+  scrollTrigger: {
+    trigger:"#page2 #circle",
+    scroller:"body",
+    // markers:true,
+  }
+})
+
+gsap.from('#page3 #circle',{
+  scale:0,
+  delay:1,
+  duration:2,
+  rotate:720,
+  scrollTrigger: {
+    trigger:"#page3 #circle",
+    scroller:"body",
+    // markers:true,
+  }
+})
+
+
